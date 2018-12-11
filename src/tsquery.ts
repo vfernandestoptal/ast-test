c3aa6fa06e4edb14f2627d40cad78ee5162084b0
Vitor Fernandes <vitor.fernandes@toptal.com>
2018-12-11 20:22:26 +0000

    resolve value of calculated property names using tsquery

--- src/tsquery.ts
+++ src/tsquery.ts
@@ -3,73 +3,91 @@ import * as ts from "typescript";
 import { MethodEndpoint } from "./types";
 
 export function getApiEndpoints(tsConfigFile: string, apiTypeName: string): MethodEndpoint[] {
-  console.log("Loading project...");
-  const tsProject = tsquery.project(tsConfigFile);
-  console.log("Project files found", tsProject.length);
+  return new ApiEndpointsParser(tsConfigFile).getApiEndpoints(apiTypeName);
+}
 
-  console.log(`Searching for API description type \`${apiTypeName}\`...`);
-  const results =
-    tsProject
-      .map((sourceFile) =>
-        tsquery(sourceFile, `InterfaceDeclaration > Identifier[name="${apiTypeName}"]`, { visitAllChildren: true }),
-      )
-      .find((result) => result.length > 0) || [];
+class ApiEndpointsParser {
+  private tsProject: ts.SourceFile[];
 
-  const apiDeclaration = results.length ? results[0].parent : undefined;
-  if (apiDeclaration && ts.isInterfaceDeclaration(apiDeclaration)) {
-    const apiDeclarationType = ts.SyntaxKind[apiDeclaration.kind];
-    console.log(`Found API ${apiDeclarationType} type in \`${apiDeclaration.getSourceFile().fileName}\``);
-
-    const endpoints = ([] as MethodEndpoint[]).concat(
-      ...getProperties(apiDeclaration).map((prop) => {
-        const url = prop.name;
-        const methods = (prop.type && getProperties(prop.type).map((prop) => prop.name)) || [];
-
-        return methods.map((method) => {
-          return {
-            url,
-            method,
-          } as MethodEndpoint;
-        });
-      }),
-    );
-
-    return endpoints;
-  } else {
-    console.log(`API declaration type \`${apiTypeName}\` not found!`);
-    return [];
+  constructor(tsConfigFile: string) {
+    console.log("Loading project...");
+    this.tsProject = tsquery.project(tsConfigFile);
+    console.log("Project files found", this.tsProject.length);
   }
-}
 
-function getProperties(node: ts.Node) {
-  const members = ts.isInterfaceDeclaration(node) || ts.isTypeLiteralNode(node) ? node.members : undefined;
+  getApiEndpoints(apiTypeName: string): MethodEndpoint[] {
+    console.log(`Searching for API description type \`${apiTypeName}\`...`);
+    const apiDeclaration = this.getTypeDeclaration(apiTypeName);
+    if (apiDeclaration && ts.isInterfaceDeclaration(apiDeclaration)) {
+      const apiDeclarationType = ts.SyntaxKind[apiDeclaration.kind];
+      console.log(`Found API ${apiDeclarationType} type in \`${apiDeclaration.getSourceFile().fileName}\``);
 
-  return (
-    (members &&
-      members.filter<ts.PropertySignature>(ts.isPropertySignature).map((prop) => {
-        const name = getTextOfPropertyName(prop.name);
-        return {
-          name,
-          type: prop.type,
-        };
-      })) ||
-    []
-  );
-}
+      const endpoints = ([] as MethodEndpoint[]).concat(
+        ...this.getProperties(apiDeclaration).map((prop) => {
+          const url = prop.name;
+          const methods = (prop.type && this.getProperties(prop.type).map((prop) => prop.name)) || [];
 
-function getTextOfPropertyName(name: ts.PropertyName) {
-  if (ts.isIdentifier(name)) {
-    return name.escapedText;
-  } else if (isStringOrNumericLiteralLike(name)) {
-    return ts.escapeLeadingUnderscores(name.text);
-  } else if (ts.isComputedPropertyName(name)) {
-    // TODO: get value of computed property name
-    return isStringOrNumericLiteralLike(name.expression)
-      ? ts.escapeLeadingUnderscores(name.expression.text)
-      : ts.escapeLeadingUnderscores(name.expression.getText());
-  } else {
+          return methods.map((method) => {
+            return {
+              url,
+              method,
+            } as MethodEndpoint;
+          });
+        }),
+      );
+
+      return endpoints;
+    } else {
+      console.log(`API declaration type \`${apiTypeName}\` not found!`);
+      return [];
+    }
+  }
+
+  getTypeDeclaration(typeName: string) {
+    const results =
+      this.tsProject
+        .map((sourceFile) => tsquery(sourceFile, `Identifier[name="${typeName}"]`))
+        .find((result) => result.length > 0) || [];
+
+    return results.length ? results[0].parent : undefined;
+  }
+
+  getProperties(node: ts.Node) {
+    const members = ts.isInterfaceDeclaration(node) || ts.isTypeLiteralNode(node) ? node.members : undefined;
+    return (
+      (members &&
+        members.filter<ts.PropertySignature>(ts.isPropertySignature).map((prop) => {
+          const name = this.getTextOfPropertyName(prop.name);
+          return {
+            name,
+            type: prop.type,
+          };
+        })) ||
+      []
+    );
+  }
+
+  getTextOfPropertyName(name: ts.PropertyName) {
+    if (ts.isIdentifier(name)) {
+      return name.escapedText;
+    } else if (isStringOrNumericLiteralLike(name)) {
+      return ts.escapeLeadingUnderscores(name.text);
+    } else if (ts.isComputedPropertyName(name)) {
+      // TODO: get value of computed property name
+      if (isStringOrNumericLiteralLike(name.expression)) return ts.escapeLeadingUnderscores(name.expression.text);
+
+      const typeDeclaration = this.getTypeDeclaration(name.expression.getText());
+      if (typeDeclaration && ts.isVariableDeclaration(typeDeclaration) && typeDeclaration.initializer) {
+        return unquote(typeDeclaration.initializer.getText());
+      }
+    }
+
     throw new Error("Unknown property name");
   }
+}
+
+function unquote(value: string): string {
+  return value.replace(/(^["|'|`]|["|'|`]$)/g, "");
 }
 
 function isStringOrNumericLiteralLike(node: ts.Node): node is ts.StringLiteralLike | ts.NumericLiteral {
